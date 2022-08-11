28f1bfe068a58dc8a19cc89fcedb363dd7dc085c
Mateus Lopes Gomes <97681752+mateuslgomes@users.noreply.github.com>
2022-08-11 18:11:05 -0300

    request para exibir uma única categoria por id

--- src/main/java/br/com/aluraflix/controller/CategoriaController.java
+++ src/main/java/br/com/aluraflix/controller/CategoriaController.java
@@ -1,12 +1,18 @@
 package br.com.aluraflix.controller;
 
+import br.com.aluraflix.controller.dto.CategoriaDto;
+import br.com.aluraflix.controller.dto.VideoDto;
 import br.com.aluraflix.model.Categoria;
+import br.com.aluraflix.model.Video;
 import br.com.aluraflix.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -16,9 +22,17 @@ public class CategoriaController {
     CategoriaRepository categoriaRepository;
 
     @RequestMapping
-    public List<Categoria> videos() {
+    public List<Categoria> categorias() {
         List<Categoria> categorias = categoriaRepository.findAll();
         return categorias;
     }
 
+    @RequestMapping(path = "/{id}")
+    public ResponseEntity<CategoriaDto> categoria(@PathVariable Long id) {
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+        if (!categoria.isEmpty()) {
+            return ResponseEntity.ok(new CategoriaDto(categoria.get()));
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
---
+++ src/main/java/br/com/aluraflix/controller/dto/CategoriaDto.java
@@ -0,0 +1,58 @@
+package br.com.aluraflix.controller.dto;
+
+import br.com.aluraflix.model.Categoria;
+import br.com.aluraflix.model.Cor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+public class CategoriaDto {
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Length(max = 25, min = 3)
+    private String titulo;
+
+    @NotNull @NotEmpty
+    private Cor cor;
+
+    public CategoriaDto(String titulo, Cor cor) {
+        this.titulo = titulo;
+        this.cor = cor;
+    }
+
+    public CategoriaDto() {}
+
+    public CategoriaDto(Categoria categoria) {
+        this.id = categoria.getId();
+        this.titulo = categoria.getTitulo();
+        this.cor = categoria.getCor();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Cor getCor() {
+        return cor;
+    }
+
+    public void setCor(Cor cor) {
+        this.cor = cor;
+    }
+}
